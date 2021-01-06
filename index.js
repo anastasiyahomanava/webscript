604618b1033ad54f5bd3d718f1a516cc9d48bf2d
Nastya <nastya_mi@icloud.com>
2021-01-06 19:09:43 +0300

    fix: underline and text color for 375 and code review

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function element() {
 
 
 let downHeader = document.getElementsByClassName("header-hide");
-let block = document.getElementById ("block");
+let block = document.getElementById("block");
 window.onload = function getArrayPoint() {
   [].forEach.call(block.children, (elem) => {
     divs = elem.getBoundingClientRect()
@@ -26,57 +26,57 @@ document.addEventListener("scroll", () => {
   let pointScroll = Math.round((pageYOffset + lineY) / 100) * 100;
   switch (pointScroll) {
     case 200:
-        console.log(downHeader[0].className, pointScroll)
+      console.log(downHeader[0].className, pointScroll)
       return downHeader[0].classList.add("header_active")
 
     case 100:
-        console.log(pointScroll+"-")
+      console.log(pointScroll + "-")
       return downHeader[0].classList.remove("header_active")
 
   }
 })
 
 // **********************************************move to links for sites*************************************************
-    var linkNav = document.querySelectorAll('[href^="#"]');
-    V = 0.5;  // скорость
-    for (var i = 0; i < linkNav.length; i++) {
-      if(i <= 4){linkNav[i].addEventListener("click",() => document.getElementsByClassName("sail_menu_active")[0].classList.remove("sail_menu_active") )}
-      linkNav[i].onclick = function(){
-        document.body.classList.remove("overflow_hidden");
-        var w = window.pageYOffset,
-            hash = this.href.replace(/[^#]*(.*)/, '$1');
-            t = document.querySelector(hash).getBoundingClientRect().top,
-            start = null;
-        requestAnimationFrame(step);
-        function step(time) {
-          if (start === null) start = time;
-          var progress = time - start,
-              r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-          window.scrollTo(0,r);
-          if (r != w + t) {requestAnimationFrame(step)} else {location.hash = hash}
-        }
-        return false;
-      }
+var linkNav = document.querySelectorAll('[href^="#"]');
+V = 0.5;  // скорость
+for (var i = 0; i < linkNav.length; i++) {
+  if (i <= 4) { linkNav[i].addEventListener("click", () => document.getElementsByClassName("sail_menu_active")[0].classList.remove("sail_menu_active")) }
+  linkNav[i].onclick = function () {
+    document.body.classList.remove("overflow_hidden");
+    var w = window.pageYOffset,
+      hash = this.href.replace(/[^#]*(.*)/, '$1');
+    t = document.querySelector(hash).getBoundingClientRect().top,
+      start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start,
+        r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+      window.scrollTo(0, r);
+      if (r != w + t) { requestAnimationFrame(step) } else { location.hash = hash }
     }
+    return false;
+  }
+}
 
 //***********************************************************************************************/
-    /// chenge a variables for clienWidth <= 375 ///
-    if(document.body.clientWidth <= 375){
-      
-      let more_info = document.getElementsByClassName("more_info")[0];
+/// chenge a variables for clienWidth <= 375 ///
+if (document.body.clientWidth <= 375) {
 
-      [].forEach.call(document.getElementsByClassName("advantages__card"), (item,i) => {if(i >2 )item.classList.add("hidden")});
+  let more_info = document.getElementsByClassName("more_info")[0];
 
-      more_info.addEventListener("click", function(){
-          
-        [].forEach.call(document.getElementsByClassName("advantages__card"),(item,i) => { 
-          item.classList.remove("hidden");
-        });
+  [].forEach.call(document.getElementsByClassName("advantages__card"), (item, i) => { if (i > 2) item.classList.add("hidden") });
 
-        more_info.style = "display: none";
+  more_info.addEventListener("click", function () {
 
-      }); 
+    [].forEach.call(document.getElementsByClassName("advantages__card"), (item, i) => {
+      item.classList.remove("hidden");
+    });
 
-    }else{
-      document.getElementsByClassName("more_info")[0].style.display = "none"
-    }
+    more_info.style = "display: none";
+
+  });
+
+} else {
+  document.getElementsByClassName("more_info")[0].style.display = "none"
+}
--- slider.js
+++ slider.js
@@ -1,43 +1,40 @@
-$(document).ready(function() {
-    
-    var owl = $(".owl-carousel");
+$(document).ready(function () {
 
-    owl.owlCarousel({
-        items : 3, 
-        margin: 10,
-        responsive:{
-            0:{
-              items:1
-            },
-            768:{
-              items:2
-            },
-            1024:{
-              items:3
-            }
-          },
-        loop: true,
-        nav: true
+  var owl = $(".owl-carousel");
 
-    });
-
-    //remove nav and dots on portfolio slider
-    $(".owl-nav:eq(1)").remove();
-    $(".owl-dots:eq(1)").remove();
-
-    if(document.body.clientWidth <= 768){
-      $(".owl-nav").remove();
-    }
-
-    /* $(".next_button").click(function(){
-      owl.trigger("next.owl.carousel");
-    });
-     
-    $(".prev_button").click(function(){
-      owl.trigger("prev.owl.carousel");
-    }); */
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1024: {
+        items: 3
+      }
+    },
+    loop: true,
+    nav: true
 
   });
 
+  //remove nav and dots on portfolio slider
+  $(".owl-nav:eq(1)").remove();
+  $(".owl-dots:eq(1)").remove();
 
- 
+  if (document.body.clientWidth <= 768) {
+    $(".owl-nav").remove();
+  }
+
+  /* $(".next_button").click(function(){
+    owl.trigger("next.owl.carousel");
+  });
+
+  $(".prev_button").click(function(){
+    owl.trigger("prev.owl.carousel");
+  }); */
+
+});
